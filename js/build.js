94c4c537aec4bf2300c2dbca2b0a38ffa80216d6
Drew-Garvey <artman.drew@gmail.com>
2016-02-22 19:18:19 -0500

    added full screen canvas, begining to resolve positioning issues.

---
+++ js/build.js
@@ -0,0 +1,16 @@
+// reference to canvas
+var can = document.getElementById("poop");
+
+function resizeCanvas() {
+  can.style.width = window.innerWidth + "px";
+  // artifical delay so innerHeight is correct
+  setTimeout(function() {
+    can.style.height = window.innerHeight + "px";
+  }, 0);
+};
+
+// Webkit/Blink will fire this on load, but Gecko doesn't.
+window.onresize = resizeCanvas;
+
+// So we fire it manually...
+resizeCanvas();
