f12d9914f96e4e261068b6283274af4573b3081e
Drew-Garvey <artman.drew@gmail.com>
2016-02-25 23:19:15 -0500

    moved all js into build.js, cleaned up html, revised css, initial launch of site.

--- js/build.js
+++ js/build.js
@@ -1,16 +1,65 @@
-// reference to canvas
-var can = document.getElementById("poop");
+(function() {
 
-function resizeCanvas() {
-  can.style.width = window.innerWidth + "px";
-  // artifical delay so innerHeight is correct
-  setTimeout(function() {
-    can.style.height = window.innerHeight + "px";
-  }, 0);
+// Shout out to Pt for being dope! [http://williamngan.github.io/pt/]
+
+// ======== Begin Pt Code ========
+
+//// 1. Define Space and Form
+var colors = {
+  a1: "#00FA9A", a2: "#00FA9A"
 };
+var space = new CanvasSpace("demo", "#F5F5DC" ).display();
+var form = new Form( space );
 
-// Webkit/Blink will fire this on load, but Gecko doesn't.
-window.onresize = resizeCanvas;
 
-// So we fire it manually...
-resizeCanvas();
+//// 2. Create Elements
+var mouse = new Circle( space.size.$divide(2) ).setRadius(200);
+var pts = [];
+for (var i=0; i<500; i++) {
+  pts.push( space.size.$multiply( Math.random(), Math.random()) );
+}
+
+//// 3. Visualize, Animate, Interact
+space.add({
+  animate: function(time, fps, context) {
+
+    // change mouse radius
+    mouse.setRadius( mouse.y/space.size.y * 200 + 8000 );
+    form.stroke(false);
+
+    // go through each point
+    for (var i=0; i<pts.length; i++) {
+
+      var size = 0.5;
+      var _p = pts[i].clone();
+
+      // if intersecting with mouse circle, change its size and scale from mouse anchor point
+      if (mouse.intersectPoint( pts[i]) ) {
+        var dist =  (mouse.radius - pts[i].distance( mouse )) / mouse.radius;
+        size = dist * 20;
+        form.fill( colors["a"+(i%4+1)] );
+        _p.scale2D( 1+dist, 1+dist, mouse );
+
+      } else {
+        form.fill( "#006699" );
+      }
+
+      // draw points
+      form.point( _p, size, true );
+    }
+  },
+  onMouseAction: function(type, x, y, evt) {
+    if (type=="move") {
+      mouse.set(x,y);
+    }
+  }
+});
+
+
+// 4. Start playing
+space.bindMouse();
+space.play(); 
+
+
+})();
+
